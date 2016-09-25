6d8608c1219615903c9c62b66f9356ab12b67fbd
Henry Wallace <henrywallace85@gmail.com>
2016-09-25 18:17:40 -0400

    Add some comments

--- learn.py
+++ learn.py
@@ -1,28 +1,56 @@
-from zipfile import ZipFile
-from music21 import midi, stream, note, pitch, duration
-from collections import defaultdict
-from itertools import islice
 import random
+from collections import defaultdict
 from functools import wraps
+from itertools import islice
+from zipfile import ZipFile
+
+from music21 import duration, midi, note, pitch, stream
 
 
 def midis_from_zip(zip_path):
+    '''Read midi files from a zip file.
+
+    TODO: Have this function read midis from all of directories, '.tar.gz',
+        '.zip', etc.
+
+    Args:
+        zip_path: Path to zip file.
+
+    Returns:
+        List of `music21.midi.MidiFile`s.
+    '''
     mfs = []
     with ZipFile(zip_path) as zip:
         for filename in (f.filename for f in zip.filelist):
             with zip.open(filename) as f:
                 mf = midi.MidiFile()
-                mf.file = f
+                mf.file = f  # with context will close f
                 mf.read()
             mfs.append(mf)
     return mfs
 
 
 def ngrams(it, n=2):
+    '''Yield ngram tuples from iterable.
+
+    Args:
+        it: Iterable to yield ngrams from. Note that this can safely be a
+            generator.
+    
+    Yields:
+        Tuples from iterable. For example:
+
+        >>> list(ngrams('trigrams', 3))
+        [('t', 'r', 'i'), ('r', 'i', 'g'), ('i', 'g', 'r'), ('g', 'r', 'a'),
+         ('r', 'a', 'm'), ('a', 'm', 's')] 
+    '''
     it = list(it)
     yield from zip(*(islice(it, i, None) for i in range(n)))
 
+
 def pitch_to_note(func):
+    '''TODO: don't use this.
+    '''
     @wraps(func)
     def wrapper(*args, **kwargs):
         pitch = func(*args, **kwargs)
@@ -30,11 +58,14 @@ def pitch_to_note(func):
         return n
     return wrapper
 
+
 class State(object):
+    '''Representation of states in Markov transition.
+    '''
     def __init__(self, pitch):
         self.pitch = pitch
-        
-        # state->count
+
+        # state -> count
         self.target_counts = defaultdict(int)
         self.target_probs = {}
 
@@ -46,7 +77,7 @@ class State(object):
         for key, value in self.target_counts.items():
             self.target_probs[key] = value / z
         return self.target_probs
-        
+
     @pitch_to_note
     def sample_transition(self):
         x = random.random()
@@ -55,13 +86,14 @@ class State(object):
                 return key
             x -= prob
         return key
-        
+
+
 def transitions(notes, n=2):
+    '''Learn transitions from notes.
+    '''
     note_info = (note.pitch for note in notes)
     transitions = ngrams(note_info, n=n)
 
-    #counts = Counter(ngrams(note_info, n=2))
-    
     states = {}
     for t in transitions:
         *source, target = t
@@ -72,7 +104,7 @@ def transitions(notes, n=2):
 
     for state in states.values():
         state.compute_probabilities()
-        
+
     return states
 
 
@@ -88,9 +120,20 @@ def sample_markov(source_notes, states, num_notes=100, dur=1, n=2):
     return notes
 
 
-n = 7
-scores = [midi.translate.midiFileToStream(m) for m in midis]
-notes = sum((notes_from_part(score[0]) for score in scores), [])
-states = transitions(notes, n=n)
-new_notes = sample_markov(notes, states, num_notes=200, dur=0.5, n=n)
-# play_notes(new_notes)     
+def notes_from_part(part):
+    return [n for n in part
+            if isinstance(n, note.Note)]
+
+
+def save_midi():
+    pass
+
+
+if __name__ == '__main__':
+    midis = midis_from_zip('bwv1007.zip')
+    n = 7
+    scores = [midi.translate.midiFileToStream(m) for m in midis]
+    notes = sum((notes_from_part(score[0]) for score in scores), [])
+    states = transitions(notes, n=n)
+    new_notes = sample_markov(notes, states, num_notes=200, dur=0.5, n=n)
+    # play_notes(new_notes)     
